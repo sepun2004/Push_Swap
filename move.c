3a7d75cb68c02377f2e56a9e22acd614169dabff
sepun2004 <sebastianpunperez@gmail.com>
2024-08-20 13:05:37 +0200

    subida

--- move.c
+++ move.c
@@ -6,13 +6,13 @@
 /*   By: sepun <sepun@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 17:45:51 by sepun             #+#    #+#             */
-/*   Updated: 2024/08/15 18:11:55 by sepun            ###   ########.fr       */
+/*   Updated: 2024/08/17 15:10:08 by sepun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	swap(t_stack *top)
+void	swap(t_list *top,  char c)
 {
 	int	tmp;
 
@@ -21,4 +21,34 @@ void	swap(t_stack *top)
 	tmp = top->content;
 	top->content = top->next->content;
 	top->next->content = tmp;
+	// if (c == 'a')
+	// 	write(1, "sa\n", 3);
+	// else if (c == 'b')
+	// 	write(1, "sb\n", 3);
+}
+
+void	push(t_list **src, t_list **dest)
+{	
+	t_list	*tmp;
+
+	if (*src == NULL)
+		return ;
+	tmp = *src;
+	*src = (*src)->next;
+	tmp->next = NULL;
+	ft_lstadd_front(dest, tmp);
+}
+
+void	rotate(t_list **top)
+{
+	t_list	*tail;
+	t_list	*tmp;
+
+	if (*top == NULL || (*top)->next == NULL)
+		return ;
+	tmp = *top;
+	*top = (*top)->next;
+	tail = ft_lstlast(tmp);
+	tail->next = tmp;
+	tmp->next = NULL;
 }
